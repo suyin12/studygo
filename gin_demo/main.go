7b4d6d5229fea5b2c7093030cbc8f77e7135e2e6
sujianhui <su_yin12@qq.com>
2023-05-04 10:58:42 +0800

    gin框架模板初识

--- gin_demo/main.go
+++ gin_demo/main.go
@@ -1,9 +1,36 @@
 package main
 
-import(
+import (
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	
+	r := gin.Default()
+
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"method": "GET",
+		})
+	})
+
+	r.POST("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"method": "POST",
+		})
+	})
+
+	r.PUT("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"method": "PUT",
+		})
+	})
+
+	r.DELETE("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"method": "DELETE",
+		})
+	})
+
+	//启动Http服务，默认在0.0.0.0:8080启动服务
+	r.Run()
 }
